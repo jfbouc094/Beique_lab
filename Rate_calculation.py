6b8d3c76586db7e01c9fd469b38c283281885074
Jean-François Boucher <j.f.boucher02@gmail.com>
2018-08-22 12:58:19 -0400

    Flow rate

---
+++ Rate_calculation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 22 09:31:07 2018
+
+@author: jean-francoisboucher
+"""
+import time
+import numpy as np
+
+import RPi.GPIO as GPIO
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25,GPIO.OUT)
+
+start_time = time.time()
+
+try:
+    while True:
+        GPIO.output(25,True)     # Turn on water
+        print('Start')
+except KeyboardInterrupt:
+    print ("Done")
+    GPIO.output(25,False)
+    GPIO.cleanup()             
+    length = time.time()-start_time
+    print('Length', (np.around(length,2)))
+
