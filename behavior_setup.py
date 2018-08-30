b45febb39fe5b9032e5d3ceb0534c9cd52ede60d
Jean-François Boucher <j.f.boucher02@gmail.com>
2018-08-30 16:07:09 -0400

    New Update

--- behavior_setup.py
+++ behavior_setup.py
@@ -40,7 +40,6 @@ class stim(object):
     def start_finish(self):
         #Set up a TTl pulse to trigger the start and finish of recording period
         GPIO.output(self.pin,True)
-        time.sleep(2)
         GPIO.output(self.pin,False)
         
         
